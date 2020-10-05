775c8e2ee2588388c01d86541ed3d199911f3a44
adhamlucas <adhamlucas20@gmail.com>
2020-10-05 03:46:16 -0400

    install cors and use in app

--- api-recomendacoes/index.js
+++ api-recomendacoes/index.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const cors = require('cors');
 const routes = require('./src/routes/Routes');
+const port = 3001;
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
-
-// const main = async() => {
-//   console.time('timerIndex');
-//   let algo = await ShowCase.getShowCases(70).catch((reason) => {
-//     console.log('ola',reason)
-//     console.log("DENTRO DO INDEX.JS")
-//   })
-//   console.timeEnd('timerIndex');
-//   return algo;
-// }
-// main().then((value) => console.log(value.mostPopular.length));
 
 app.listen(port, () => {
   console.log(`The server is runing on: http://localhost:${port}`)
